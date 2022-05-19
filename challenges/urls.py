7489e385a0eee37426e15a6ddb47f787da10cd74
dmawardi <dmawardi@gmail.com>
2022-05-19 12:35:19 +1000

    Completed views and urls. No templates yet.

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,12 +3,13 @@ from . import views
 
 # List of url patterns
 urlpatterns = [
+    path("", views.index),
     # Uses path function to create path of /january and uses views.index to resolve response
     # path("january", views.january),
     # path("february", views.february),
     # dynamic path with parameter. Allows for type casting
     path("<int:month>", views.monthly_challenge_by_number),
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="str-monthly-challenge")
 
 
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,7 @@ from http.client import HTTPResponse
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     # dictionaries are ordered in Python 3
@@ -19,6 +20,22 @@ monthly_challenges = {
     "december": "Walk for at least 20 minutes every day!",
 }
 
+
+def index(request):
+    # Init list items
+    list_items = ""
+
+    # Loop through months of monthly challenges
+    for month in monthly_challenges.keys():
+        # Generate URL for link
+        month_path = reverse("str-monthly-challenge", args=[month])
+        # Build list item
+        list_items += f"<li><a href=\"{month_path}\">{month.capitalize()}</a></li>"
+
+    # Set list items in unordered list
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 # Functions in views can accept the request object for usage within
 # def january(request):
 #     # You can pass response data to client using Http Response
@@ -35,9 +52,10 @@ def monthly_challenge(request, month):
     print("Monthly challenge request: " + month)
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound("This month is not supported")
+        return HttpResponseNotFound("<h1>This month is not supported</h1>")
 
 
 def monthly_challenge_by_number(request, month):
@@ -50,6 +68,9 @@ def monthly_challenge_by_number(request, month):
         return HttpResponseNotFound("Invalid month")
 
     # grab month by index
-    forward_month = months[month-1]
+    redirect_month = months[month-1]
+    # reverse allows you to create paths by using path names declared in urls
+    # also accepts args
+    redirect_path = reverse("str-monthly-challenge", args=[redirect_month])
     # redirect to new url using string verison of month in URL
-    return HttpResponseRedirect('/challenges/'+forward_month)
+    return HttpResponseRedirect(redirect_path)
